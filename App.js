1767ff014e93a2db943e7fbc5fa15338e8e1f72b
Mariglen Jahaj <31398575+mJahaj@users.noreply.github.com>
2023-06-09 23:40:36 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
-  const message = "Hello Gane";
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>{message}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8 </Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.descripton}>Its Sunny</Text>
+        <Text style={styles.message}>Its Perfect T-shirt weather</Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+   
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
   },
+  wrapper: {
+    flex: 1,
+    backgroundColor: "pink",
+  },
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+    color: "black",
+  },
+  highLow: {
+    color: "black",
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: "row",
+  },
+  bodyWrapper: {
+    justifyContent: "flex-end", //places content to bottom
+    alignItems: "flex-start", //left of screen
+  },
+  descripton: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
 });
+
+export default App;
