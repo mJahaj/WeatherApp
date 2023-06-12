afdbaedf00a302160ad6f4f719241267c15e1901
Mariglen Jahaj <31398575+mJahaj@users.noreply.github.com>
2023-06-12 01:50:39 -0400

    added some styles to tab navigation
    added custom icons for tabs and color for active or inactive tab

--- App.js
+++ App.js
@@ -1,28 +1,58 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Text } from "react-native";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator(); // init Tab Object
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      screenOptions={{
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "grey",
+      }}
+    >
       <Tab.Navigator>
-        <Tab.Screen name={"Current"} component={CurrentWeather} />
-        <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
-        <Tab.Screen name={"City"} component={City} />
+        <Tab.Screen
+          name={"Current"}
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: (
+              { focused } // if tab is focused
+            ) => (
+              <Feather
+                name={"droplet"}
+                size={25}
+                color={focused ? "tomato" : "black"} // icon color is tomato if focused otherwise black
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={"Upcoming"}
+          component={UpcomingWeather}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <Feather
+                name={"clock"}
+                size={25}
+                color={focused ? "tomato" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen name={"City"} component={City} options={{
+          tabBarIcon: ({focused}) => (
+            <Feather name={"home"} size={25} color={focused ? 'tomato' : 'black'}/>
+          )
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+  );
+};
 
 export default App;
