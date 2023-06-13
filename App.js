b6772cf5db9dc68462bd82c3a7cbcf714bfddab0
Mariglen Jahaj <31398575+mJahaj@users.noreply.github.com>
2023-06-13 00:53:03 -0400

    practice hooks
    created counter.js to understand hoe useState and useEffect work

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 
-
 const App = () => {
   return (
     <NavigationContainer>
---
+++ src/demonstration/Counter.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+
+const Counter = () => {
+
+  const [count, setCount] = useState(0)
+  const [newCount, setNewCount] = useState(0)
+
+  useEffect(() => {
+    console.log(`count changed`)
+    return () => {console.log('UseEffect cleanup')}
+  }, [count, newCount])// dependency arr specifes what data needs to change for a re-render
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{`count: ${count}`}</Text>
+      <Button
+        color={"red"}
+        title={"Increase the count"}
+        onPress={() => 
+           
+          setCount(count + 1)
+        }
+      />
+      <Button
+        color={"green"}
+        title={"Decrease count"}
+        onPress={() => setCount(count-1) }
+      />
+           <Button
+        color={"red"}
+        title={"Increase the count"}
+        onPress={() => 
+           
+          setNewCount(newCount + 1)
+        }
+      />
+      <Button
+        color={"green"}
+        title={"Decrease count"}
+        onPress={() => setNewCount(newCount-1) }
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'orange'
+    },
+    title: {
+        alignSelf: "center",
+        fontSize: 25,
+        marginTop: 25
+    }
+});
+export default Counter;
